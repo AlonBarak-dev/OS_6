e51dffcd8511408e512b2eb7498b86042cf3cc71
idan philosoph <edanphi@gmail.com>
2022-05-15 14:24:54 +0300

    added all the files

---
+++ Q.cpp
@@ -0,0 +1,17 @@
+#include "OS6\\Q.hpp"
+
+class Q {
+    public:
+        Q() {
+            
+        }
+        ~Q() {
+
+        }
+        void enQ(void* value) {
+
+        }
+        void* deQ() {
+
+        }
+};
